712ae9b206e5fd2ba4b1ada12ce85b5a840343de
jcw1012 <jiang1802015@gmail.com>
2025-05-06 23:40:02 +0800

    Create 1.js
    替换西数深圳入口

---
+++ 1.js
@@ -0,0 +1,16 @@
+function operator(proxies) {
+  const targetIP = "113.108.90.36";
+  
+  return proxies.map(proxy => {
+    // 只处理 Shadowsocks 节点
+    if (proxy.type === "trojan") {
+      // 检查节点名称是否包含 "IPLC" 或 "移动"
+      if (proxy.server.includes("guangzhou")) 
+   {
+        // 替换 IP 地址
+        proxy.server = targetIP;
+      }
+    }
+    return proxy;
+  });
+}
